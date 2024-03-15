19f5db790ddf3b08688340d71a6a7a37e1137080
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-14 20:58:54 -0400

    Created main.py - the python file where our program will be coded base on

---
+++ main.py
@@ -0,0 +1,14 @@
+#Semester Project - Password generator
+import random
+
+#Provide the option for user to choose to enter string to include
+# in the generated password
+print()
+
+#main function
+def main():
+    print("Enter the whatever you want to include in the generated password\n")
+    option = input("or leave blank.\n")
+
+#----------------------------------------------
+main()
