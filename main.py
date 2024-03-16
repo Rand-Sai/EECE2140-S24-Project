93cc1c9480197215d218b0827679574b5c3639c4
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-16 17:06:11 -0400

    Coded basic structure for the QRcode generator class, completed the generate qr_codefunction, waiting for test and evaluation

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 #Semester Project - QR Code generator
 import qrcode
 
+class QRCodeGenerator:
+    def __init__(self):
+        self.qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+    def generate_qr_code(self, url):
+        self.qr.clear()
+        self.qr.add_data(url)
+        self.qr.make(fit=True)
+        
 #main function
 def main():
     #Greet user
