b1b17551b051a5e26e355d64196f4c0b791e947e
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-27 11:49:09 -0400

    Upgrade main.py - generate_qr_code function, now user may save the qrcode png with name they want

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class QRCodeGenerator:
             box_size=10,
             border=4,
         )
-    def generate_qr_code(self, url):
+    def generate_qr_code(self, url, name1=None):
         self.qr.clear()
         self.qr.add_data(url)
         self.qr.make(fit=True)
@@ -19,7 +19,10 @@ class QRCodeGenerator:
         img = self.qr.make_image(fill_color="black", back_color="white")
 
         # Save the image with a name based on the URL
-        img_name = url.replace("://", "_").replace("/", "_") + ".png"
+        if name1 == None:
+            img_name = url.replace("://", "_").replace("/", "_") + ".png"
+        else:
+            img_name = str(name1) + ".png"
         img.save(img_name)
         print(f"QR code generated successfully as {img_name}")
         
@@ -27,8 +30,9 @@ class QRCodeGenerator:
 def main():
     #Greet user
     print('Hello!\n')
-    url1 = input("Enter the url of the website: \n")
+    url1 = input("Enter the url of the website or any text: \n")
+    name1 = input("Enter the name of this QRcode image or leave blank: \n")
     QRG = QRCodeGenerator()
-    QRG.generate_qr_code(url1)
+    QRG.generate_qr_code(url1, name1)
 #----------------------------------------------
 main()
