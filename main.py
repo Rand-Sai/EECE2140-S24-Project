04e60aafd2f49e626952fe44be3acb89c75a1734
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-27 12:15:59 -0400

    Add color changing feature for user, now user may change the color of their qrcode

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ class QRCodeGenerator:
             box_size=10,
             border=4,
         )
-    def generate_qr_code(self, url, name1=None):
+    def generate_qr_code(self, url, name=None, color = "black"):
         self.qr.clear()
         self.qr.add_data(url)
         self.qr.make(fit=True)
 
         
         # Create an image from the QR Code instance
-        img = self.qr.make_image(fill_color="black", back_color="white")
+        img = self.qr.make_image(fill_color=color, back_color="white")
 
         # Save the image with a name based on the URL
-        if name1 == None:
+        if name == None:
             img_name = url.replace("://", "_").replace("/", "_") + ".png"
         else:
-            img_name = str(name1) + ".png"
+            img_name = str(name) + ".png"
         img.save(img_name)
         print(f"QR code generated successfully as {img_name}")
         
@@ -32,7 +32,8 @@ def main():
     print('Hello!\n')
     url1 = input("Enter the url of the website or any text: \n")
     name1 = input("Enter the name of this QRcode image or leave blank: \n")
+    color1 = input("Enter the color of this QRcode image or leave blank: \n")
     QRG = QRCodeGenerator()
-    QRG.generate_qr_code(url1, name1)
+    QRG.generate_qr_code(url1, name1, color1)
 #----------------------------------------------
 main()
